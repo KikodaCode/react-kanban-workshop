c1d6fecaed3ecdf6dad30280852855ff326e384b
Robert Hodgen <roberthodgen@gmail.com>
2019-03-27 16:45:51 -0400

    Add API fetch

--- app/service/kanbanBoard.js
+++ app/service/kanbanBoard.js
@@ -1,43 +1,6 @@
-export default () => ({
-  name: 'Kanban Board',
-  columns: [
-    {
-      name: 'Backlog',
-      color: 'blue',
-    },
-    {
-      name: 'In Progress',
-      color: 'yellow',
-    },
-    {
-      name: 'Blocked',
-      color: 'red',
-    },
-    {
-      name: 'Done',
-      color: 'green',
-    },
-  ],
-  tasks: [
-    {
-      id: 1,
-      status: 'In Progress',
-      description: 'Make a Kanban board.',
-    },
-    {
-      id: 2,
-      status: 'Done',
-      description: 'Style Kanban board.',
-    },
-    {
-      id: 3,
-      status: 'Backlog',
-      description: 'Write tests.',
-    },
-    {
-      id: 4,
-      status: 'Backlog',
-      description: 'Persist tasks.',
-    },
-  ],
-});
+export default async function() {
+  const response = await fetch('http://workshops.kikoda.com/kanban.json');
+  if (!response.ok)
+    throw new Error(`Fetch failed with status code ${response.status}`);
+  return response.json();
+}
